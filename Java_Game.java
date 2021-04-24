374a32c895f80aac6b85d22a92050b9f1a3c9f89
Ben Foster <31930426+Benfoster180@users.noreply.github.com>
2021-04-24 14:51:08 +0100

    Fixed Issue
    Before the image would not load when the game was first loaded,

--- Java_Game.java
+++ Java_Game.java
@@ -5,7 +5,8 @@ screen = pygame.display.set_mode((500,500))
 Background_day = "Backgroundatdaytime.png"
 Background_night = "Cityatnight.png"
 
-
+background_image = pygame.image.load("Backgroundatdaytime.png").convert()
+screen.blit(background_image, [0, 0])
 # Changes the Time in game based on a loop and sleep commands 
 while num == 1:
   background_image = pygame.image.load("Backgroundatdaytime.png").convert()
@@ -17,6 +18,7 @@ while num == 1:
   pygame.display.update()
   screen.blit(background_image, [0, 0])
   time.sleep(150)
+
 
 
 pygame.display.flip()
