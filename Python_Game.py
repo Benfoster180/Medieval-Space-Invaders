d1c460ae99cdb3f0d1f94e7971e06fbef54173f9
Cursed <31930426+Benfoster180@users.noreply.github.com>
2021-05-02 13:14:27 +0100

    Added the following
    1) Shooting
    2) A cool down for shooting
    3)A Rock class
    4)New action for player

--- Python_Game.py
+++ Python_Game.py
@@ -8,7 +8,7 @@ fps = 60
 screen_width = 500
 screen_height = 500
 
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width,screen_height)
 pygame.display.set_caption('Game Name')
 
 
