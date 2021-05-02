e9df88f21354246ccf23d31d0791d00a23326b6b
Cursed <31930426+Benfoster180@users.noreply.github.com>
2021-05-02 12:32:10 +0100

    Updated
    Added the following
    1) Sprite
    2) Sprite movement
    3)Limits to stop the sprite going off the map

--- Python_Game.py
+++ Python_Game.py
@@ -1,24 +1,74 @@
 import pygame 
-import time
-num = 1
-screen = pygame.display.set_mode((500,500))
-Background_day = "Backgroundatdaytime.png"
-Background_night = "Cityatnight.png"
+from pygame.locals import *
 
-background_image = pygame.image.load("Backgroundatdaytime.png").convert()
-screen.blit(background_image, [0, 0])
-# Changes the Time in game based on a loop and sleep commands 
-while num == 1:
-  background_image = pygame.image.load("Backgroundatdaytime.png").convert()
-  print("1")
+#FPS Settings
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 500
+screen_height = 500
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Game Name')
+
+
+#load image
+bg = pygame.image.load("Game_backdrop.png")
+
+def draw_bg():
+  screen.blit(bg, (0,0))
+
+#This is where we create the player
+class Player(pygame.sprite.Sprite):
+  def __init__(self,x,y):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.image.load("Catapult.png")
+    self.rect = self.image.get_rect()
+    self.rect.center = [x,y]
+  
+  def update(self):
+    #Speed of the object
+    speed = 8
+
+    #reaction to key press
+    key = pygame.key.get_pressed()
+    
+    # pLAYER MOVMENT LEFT
+    if key[pygame.K_LEFT] and self.rect.left > 0:
+      self.rect.x -= speed
+
+    #Player movemtn right  
+    if key[pygame.K_RIGHT] and self.rect.right < screen_width:
+      self.rect.x += speed
+
+
+#Sprite Groups
+Player_group = pygame.sprite.Group()
+
+
+#Create
+PLAYER = Player(int(screen_width/2), screen_height - 100)
+Player_group.add(PLAYER)
+
+
+
+run = True
+
+while run:
+  #Game code in Here 
+  draw_bg()
+  clock.tick(fps)
+  #event handlers
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        run = False
+
+  #Player upade
+  PLAYER.update()
+
+  #Sprite group draw
+  Player_group.draw(screen)
+
+
   pygame.display.update()
-  screen.blit(background_image, [0, 0])
-  time.sleep(150)
-  background_image = pygame.image.load("Cityatnight.png").convert()
-  pygame.display.update()
-  screen.blit(background_image, [0, 0])
-  time.sleep(150)
-
-
-
-pygame.display.flip()
+pygame.quit()
