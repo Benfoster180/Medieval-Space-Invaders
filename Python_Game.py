cd5c4926599a71935e6b9aa8553f2a5de28208b2
Cursed <31930426+Benfoster180@users.noreply.github.com>
2021-05-02 13:50:51 +0100

    Added the following
    1)Bad guys 
    2)Added movment for them

--- Python_Game.py
+++ Python_Game.py
@@ -1,15 +1,25 @@
 import pygame 
 from pygame.locals import *
+import random 
 
 #FPS Settings
 clock = pygame.time.Clock()
 fps = 60
 
-screen_width = 500
-screen_height = 500
+screen_width = 600
+screen_height = 800
 
-screen = pygame.display.set_mode((screen_width,screen_height)
+screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Game Name')
+
+#Game reverb
+rows = 5
+cols = 5
+
+#colors 
+BLACK = (0,0,0)
+RED = (255,0,0)
+
 
 
 #load image
@@ -20,16 +30,20 @@ def draw_bg():
 
 #This is where we create the player
 class Player(pygame.sprite.Sprite):
-  def __init__(self,x,y):
+  def __init__(self,x,y, health):
     pygame.sprite.Sprite.__init__(self)
     self.image = pygame.image.load("Catapult.png")
     self.rect = self.image.get_rect()
     self.rect.center = [x,y]
-  
+    self.health_start = health 
+    self.health_remaining = health
+    self.last_peew_peew = pygame.time.get_ticks()
+
   def update(self):
     #Speed of the object
     speed = 8
-
+    #Shooting cool down
+    cooldown = 500 #Millersecs
     #reaction to key press
     key = pygame.key.get_pressed()
     
@@ -42,12 +56,71 @@ class Player(pygame.sprite.Sprite):
       self.rect.x += speed
 
 
+    #CURREnt time
+    Current_time = pygame.time.get_ticks()
+    #peew peww button (shooting)
+    if key[pygame.K_SPACE] and Current_time - self.last_peew_peew > cooldown:
+      ROCK = Rock(self.rect.centerx, self.rect.top)
+      Rock_group.add(ROCK)
+      self.last_peew_peew = Current_time
+
+  #Health bar
+    pygame.draw.rect(screen, BLACK,(self.rect.x, (self.rect.bottom + 10), self.rect.width,15))
+    if self.health_remaining > 0:
+      pygame.draw.rect(screen, RED,(self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining / self.health_start)), 15))
+
+
+#Create the rock
+class Rock(pygame.sprite.Sprite):
+  def __init__(self,x,y):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.image.load("Rock.png")
+    self.rect = self.image.get_rect()
+    self.rect.center = [x,y]
+  
+  def update(self):
+    self.rect.y -= 5
+    if self.rect.bottom < 0:
+      self.kill()
+
+
+#Ballons
+
+class Ballons(pygame.sprite.Sprite):
+  def __init__(self,x,y):
+    pygame.sprite.Sprite.__init__(self)
+    #Named ballons like Ballon1
+    self.image = pygame.image.load("Ballon" + str(random.randint(1,1)) + ".png")
+    self.rect = self.image.get_rect()
+    self.rect.center = [x,y]
+    self.move_counter = 0
+    self.move_direction = 1
+  
+  def update(self):
+    self.rect.x += self.move_direction
+    self.move_counter += 1
+    if abs(self.move_counter) > 75:
+      self.move_direction *= -1
+      self.move_counter *= self.move_direction
+
+
+
 #Sprite Groups
 Player_group = pygame.sprite.Group()
+Rock_group = pygame.sprite.Group()
+Ballon_group = pygame.sprite.Group()
 
+def create_ballons():
+  #gen ballom
+  for row in range(rows):
+    for item in range(cols):
+      BALLON = Ballons(100 + item * 100, 100 + row * 70)
+      Ballon_group.add(BALLON)
+      
+create_ballons()
 
 #Create
-PLAYER = Player(int(screen_width/2), screen_height - 100)
+PLAYER = Player(int(screen_width/2), screen_height - 100, 3)
 Player_group.add(PLAYER)
 
 
@@ -68,7 +141,12 @@ while run:
 
   #Sprite group draw
   Player_group.draw(screen)
+  Rock_group.draw(screen)
+  Ballon_group.draw(screen)
 
+  #Group updates
+  Rock_group.update()
+  Ballon_group.update()
 
   pygame.display.update()
 pygame.quit()
